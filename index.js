bacb2e1220fa00ce8f394979ab92c8997e1d82e7
riffle <mriffle@stashimi.com>
2017-08-18 15:26:39 -0700

    Built-in permissions, Customizable loghandling, backwards compatible

--- index.js
+++ index.js
@@ -29,21 +29,45 @@ class LogStreamingPlugin {
 	}
 
 	logServerless() {
+		let logHandlerLambdaName;
+		const logHandlerFnName = _.get(this.serverless, "service.custom.logHandler.function", "loghandler");
 		_.forEach(this.serverless.service.getAllFunctions(), functionName => {
-			if (functionName === "loghandler") {
+			const functionLogicalId = `${_.upperFirst(functionName)}LambdaFunction`;
+			if (functionName === logHandlerFnName) {
+				// Do not add the loghandler to itself.
+				logHandlerLambdaName = functionLogicalId;
 				return;
 			}
-			const functionLogicalId = `${_.upperFirst(functionName)}LambdaFunction`;
 			const functionObject = this.serverless.service.getFunction(functionName);
-
+			if (_.get(functionObject, "loghandler", true) === false) {
+				// If the loghandler property is set to false then do not create a loghandler
+				return;
+			}
 			cloudwatchLogsSubscriptionFilterTemplate.Properties.LogGroupName = `/aws/lambda/${functionObject.name}`;
 			const newResources = {
 				[`${functionLogicalId}SubscriptionFilter`]: cloudwatchLogsSubscriptionFilterTemplate
 			};
 
 			_.merge(this.serverless.service.provider.compiledCloudFormationTemplate.Resources, newResources);
-			
 		});
+		
+		if (!_.has(this.serverless.service.provider.compiledCloudFormationTemplate.Resources, "LoggingLambdaPermission")) {
+			if (!logHandlerLambdaName || !logHandlerFnName) {
+				throw new Error("Loghandler plugin is not properly configured. Missing loghandler function definition.");
+			}
+			const loggingPermissions = {
+				["LoggingLambdaPermission"]: {
+					Type: "AWS::Lambda::Permission",
+					Properties: {
+						FunctionName: logHandlerLambdaName,
+						Action: "lambda:InvokeFunction",
+						Principal: "logs.amazonaws.com"
+					}
+				}
+			};
+			this.serverless.cli.log(`Adding 'LoggingLambdaPermission' to Resources with FunctionName ${logHandlerLambdaName}`);
+			_.merge(this.serverless.service.provider.compiledCloudFormationTemplate.Resources, loggingPermissions);
+		}
 	}
 }
 
